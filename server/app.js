4cbd87b24b5a0458c69353365c60e31a54c839c1
ob6160 <blastdoors666@gmail.com>
2013-08-13 18:14:09 +0100

    Added Base Server Files
    Added socket.io, nodejs and express.

---
+++ server/app.js
@@ -0,0 +1,11 @@
+var express = require('express'),
+	app = express(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
+
+app.configure(function() {
+    app.use(express.static(__dirname + '../'));
+});
+
+
+server.listen(1337);
