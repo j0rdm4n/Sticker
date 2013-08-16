d238d6c0019faff0e9af057580984436c78b30e5
ob6160 <blastdoors666@gmail.com>
2013-08-16 13:02:49 +0100

    Queries The Database

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require('express'),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server);
 	db = require('./mysql_conn');
+
 app.configure(function() {
     app.use(express.static(__dirname + '/www'));
 });
+
 io.sockets.on('connection', function(socket) {
 
 });
--- mysql_conn.js
+++ mysql_conn.js
@@ -1,11 +1,11 @@
 //Require mysql connector that you installed with npm
-var mysql      = require('mysql');
+var mysql = require('mysql');
 
-var conn_conf= {
-    host     : 'localhost',
+var conn_conf = {
+    host     :'localhost',
     port     :3306,
-    user     : 'root',
-    password : '',
+    user     :'root',
+    password :'',
     database: 'sticker'
 }
 
@@ -17,3 +17,13 @@ connection.connect(function(err) {
         console.log("   Connected to database: "+conn_conf.database+' on '+conn_conf.host );
     }
 });
+connection.query( 'SELECT * FROM sticker.stickers ', function(err, rows) {
+            console.log("SELECT * FROM stickers ");
+
+            for(var i=0; i<rows.length; i++){
+                console.log(rows[i]);
+            }
+
+            return rows;
+
+    });
