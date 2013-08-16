9bf6974d632afa0bfa00c61b144d24a96369261d
ob6160 <blastdoors666@gmail.com>
2013-08-16 13:49:42 +0100

    Changed shit
    I hate callbacks so i skipped it

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require('express'),
+	mysql = require('mysql'),
 	app = express(),
+	conn_conf = {
+    host     :'localhost',
+    port     :3306,
+    user     :'root',
+    password :'',
+    database: 'sticker'
+	},
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server);
-	db = require('./mysql_conn');
+	
 
 app.configure(function() {
     app.use(express.static(__dirname + '/www'));
@@ -11,7 +19,23 @@ app.configure(function() {
 io.sockets.on('connection', function(socket) {
 
 });
-console.log(db.query('SELECT * FROM stickers'));
 
+var connection = mysql.createConnection(conn_conf);
+connection.connect(function(err) {
+    if(err) console.log("Could not connect to DB");
+    else {
+        console.log("   Connected to database: "+conn_conf.database+' on '+conn_conf.host );
+    }
+});
+connection.query( 'SELECT * FROM sticker.stickers ', function(err, rows) {
+           
 
+            for(var i=0; i<rows.length; i++){
+                console.log(rows[i]);
+            }
+
+            return rows;
+
+    });
 server.listen(8888);
+
