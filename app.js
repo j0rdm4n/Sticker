0d1fba70a80b6eaaf9a430b9178503107a81653f
ob6160 <blastdoors666@gmail.com>
2013-08-17 17:45:12 +0100

    Experimenting with ajax and node and mysql

--- app.js
+++ app.js
@@ -1,41 +1,34 @@
 var express = require('express'),
 	mysql = require('mysql'),
 	app = express(),
-	conn_conf = {
+	connection = mysql.createConnection({
     host     :'localhost',
     port     :3306,
     user     :'root',
     password :'',
     database: 'sticker'
-	},
+	}),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server);
+	app.configure(function() {
+	  app.use(express.bodyParser());
+	  app.use(express.methodOverride());
+	  app.use(app.router);
+	  app.use(express.static(__dirname + '/www'));
+	  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 	
-
-app.configure(function() {
-    app.use(express.static(__dirname + '/www'));
-});
-
-io.sockets.on('connection', function(socket) {
-
-});
-
-var connection = mysql.createConnection(conn_conf);
-connection.connect(function(err) {
-    if(err) console.log("Could not connect to DB");
-    else {
-        console.log("   Connected to database: "+conn_conf.database+' on '+conn_conf.host );
-    }
-});
-connection.query( 'SELECT * FROM sticker.stickers ', function(err, rows) {
-           
-
-            for(var i=0; i<rows.length; i++){
-                console.log(rows[i]);
-            }
-
-            return rows;
-
-    });
+	});
+	app.get('/getallusers', function (req, res) {
+	   connection.query('SELECT * FROM user_data;', function (error, rows, fields) { 
+	         res.writeHead(200, {'Content-Type': 'text/plain'});
+			 str='';
+			 for(i=0;i<rows.length;i++)
+				str = str + rows[i].a_name +'\n';
+			 res.end( str);
+	      }); 
+	});
+	io.sockets.on('connection', function(socket) {
+	
+	});
 server.listen(8888);
 
